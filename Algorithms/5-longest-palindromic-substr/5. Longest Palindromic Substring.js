d8984fb72030bf7979771c1444ee57b1f5f43523
Muhammad Majid <mmajidkamel@gmail.com>
2021-04-06 21:40:02 +0200

    WIP longest palindromic substring

---
+++ Algorithms/5-longest-palindromic-substr/5. Longest Palindromic Substring.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Expanding From Middle Solution.
+ */
+var longestPalindrome = function(s) {
+  if (s === null || s.length < 1) return "";
+
+  let start, end = 0;
+    
+};
+
+const expandFromMiddle = (s, left, right) => {
+  if (s === null || left > right) return 0;
+}
