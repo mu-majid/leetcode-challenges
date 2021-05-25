d6a4d6782856ce93a155317051b96d9d9af8495e
Muhammad Majid <mmajidkamel@gmail.com>
2021-05-25 20:00:13 +0200

    Solution: Expand Around Center for longest palindrome substr

--- Algorithms/5-longest-palindromic-substr/5. Longest Palindromic Substring.js
+++ Algorithms/5-longest-palindromic-substr/5. Longest Palindromic Substring.js
@@ -2,13 +2,33 @@
  * 
  * Expanding From Middle Solution.
  */
-var longestPalindrome = function(s) {
-  if (s === null || s.length < 1) return "";
+const longestPalindrome = (s) => {
+  if (s.length < 1) return "";
 
-  let start, end = 0;
-    
-};
+  let maxSubStart = 0;
+  let maxSubLength = 0;
 
-const expandFromMiddle = (s, left, right) => {
+  for (let i = 0; i < s.length; i++) {
+    const lengthCenteredAtChar = expandAroundCenter(s, i, i);
+    const lengthCenteredAtSpace = expandAroundCenter(s, i, i + 1);
+    const longestSubAtChar = Math.max(lengthCenteredAtChar, lengthCenteredAtSpace);
+
+    if (longestSubAtChar > maxSubLength) {
+      maxSubLength = longestSubAtChar;
+      maxSubStart = i - Math.floor((maxSubLength - 1) / 2);
+    }
+  }
+
+  return s.substr(maxSubStart, maxSubLength);
+}
+
+const expandAroundCenter = (s, left, right) => {
   if (s === null || left > right) return 0;
+
+  // Means that we start from middle and continue expanding only if we are in the string boundaries and also the characters match
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    left--;
+    right++;
+  }
+  return right - left - 1;
 }
