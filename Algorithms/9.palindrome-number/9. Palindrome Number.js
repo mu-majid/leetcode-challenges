570aa758f3953d63dbe2537bfee509ec374887be
Muhammad Majid <mmajidkamel@gmail.com>
2022-01-22 21:45:16 +0100

    Update 9. Palindrome Number.js

--- Algorithms/9.palindrome-number/9. Palindrome Number.js
+++ Algorithms/9.palindrome-number/9. Palindrome Number.js
@@ -3,5 +3,28 @@
  * @return {boolean}
  */
 const isPalindrome = function(x) {
-  
+  // -121 is not the same as 121-
+  if (x < 0) {
+    return false;
+  }
+
+  let divisor = 1;
+  while (parseInt(x / divisor) >= 10) {
+    divisor *= 10
+  }
+
+  while (x !== 0) {
+    let leading = parseInt(x / divisor);
+    let trailing = x % 10;
+
+    if (leading !== trailing) {
+      return false;
+    }
+
+    x = parseInt((x % divisor) / 10);
+
+    divisor = divisor / 100;
+  }
+
+  return true;
 };
