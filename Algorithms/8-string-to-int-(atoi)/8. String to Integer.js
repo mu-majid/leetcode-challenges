0038d293e011dddfa173a4f82906754692f80950
Muhammad Majid <mmajidkamel@gmail.com>
2022-01-22 21:31:43 +0100

    Update 8. String to Integer.js

--- Algorithms/8-string-to-int-(atoi)/8. String to Integer.js
+++ Algorithms/8-string-to-int-(atoi)/8. String to Integer.js
@@ -3,6 +3,46 @@
  * @return {number}
  */
 
-const myAtoi = function(s) {
-  
+/**
+ * This Implementation will get the first number in the string and log it
+ */
+
+const myAtoi = function (s) {
+  if (!s || s.length === 0) {
+    return null;
+  }
+
+  const trimmed = s.trimStart();
+  let negative = false;
+  let foundNumber = false;
+  let finalNum = '';
+
+  for (let index = 0; index < trimmed.length; index++) {
+    const char = trimmed[index];
+    if (char === '-') {
+      negative = true;
+    }
+    if (parseInt(char)) {
+      foundNumber = true;
+      finalNum += char;
+    }
+    if (foundNumber && !parseInt(trimmed[index + 1])) {
+      break;
+    }
+  }
+  finalNum = parseInt(finalNum);
+
+
+
+  if (finalNum >= Math.pow(2, 31)) {
+    return Math.pow(2, 31) - 1
+  } else if (finalNum <= Math.pow(-2, 31)) {
+    return Math.pow(-2, 31)
+  } else {
+    return negative ? -finalNum : finalNum;
+  }
 };
+
+console.log(myAtoi(" with words 987"));     // --> 987
+console.log(myAtoi(" 123 with words 987")); // --> 123
+console.log(myAtoi(" -24 with words 987")); // --> -24
