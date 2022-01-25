087bf53dacaab2bdd01611efc3790606c5f8f716
Muhammad Majid <mmajidkamel@gmail.com>
2022-01-25 03:44:50 +0100

    javascript solution

--- Algorithms/11-Container-with-most-water/11. Container with  most water.js
+++ Algorithms/11-Container-with-most-water/11. Container with  most water.js
@@ -4,7 +4,21 @@
  */
 const maxArea = function (height) {
 
-  const area = (x1, y1, x2, y2) => {
-    return (x2 - x1) * Math.min(y2, y1);
+  let maximumArea = Number.MIN_SAFE_INTEGER;
+  let leftPtr = 0;
+  let rightPtr = height.length - 1;
+
+  while (leftPtr < rightPtr) {
+    let shorterHeight = Math.min(height[leftPtr], height[rightPtr]);
+    maximumArea = Math.max(maximumArea, shorterHeight * (rightPtr - leftPtr))
+
+    if (height[leftPtr] < height[rightPtr]) {
+      leftPtr++;
+    }
+    else {
+      rightPtr--;
+    }
   }
+
+  return maximumArea;
 };
