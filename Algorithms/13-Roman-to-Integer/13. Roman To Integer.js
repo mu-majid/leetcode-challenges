3f5b9d242f7db90f0cc8eafd126efbd8c3a25f87
Muhammad Majid <mmajidkamel@gmail.com>
2022-01-26 12:41:17 +0100

    Solved roman to int

--- Algorithms/13-Roman-to-Integer/13. Roman To Integer.js
+++ Algorithms/13-Roman-to-Integer/13. Roman To Integer.js
@@ -3,9 +3,41 @@
  * @return {number}
  */
 
-const value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-const roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+const romanToIntMap = {
+  "M": 1000,
+  "CM": 900,
+  "D": 500,
+  "CD": 400,
+  "C": 100,
+  "XC": 90,
+  "L": 50,
+  "XL": 40,
+  "X": 10,
+  "IX": 9,
+  "V": 5,
+  "IV": 4,
+  "I": 1
+}
 
 const romanToInt = function (s) {
+  let result = 0;
+  let i = 0;
+  // loop over the roman num
+  while (i < s.length) {
+    // check for the 6 special cases and update i accordingly
+    if (romanToIntMap.hasOwnProperty(s[i] + s[i+1])) {
+      result = result + romanToIntMap[`${s[i] + s[i+1]}`];
+      i += 2
+    }
+    else {
+      result = result + romanToIntMap[s[i]];
+      i += 1
+    }
+  }
 
+  return result;
 };
+
+console.log(romanToInt("LVIII"))
+console.log(romanToInt("MCMXCIV"))
+console.log(romanToInt("III"))
