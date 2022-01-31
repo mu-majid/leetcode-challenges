f855f3f4a961c0116cb736704251b4edfcf9566b
Muhammad Majid <mmajidkamel@gmail.com>
2022-01-31 21:04:24 +0100

    BFS with AdjList Graph

--- Data-Structures/Graph/Graph-AdjList.js
+++ Data-Structures/Graph/Graph-AdjList.js
@@ -50,6 +50,43 @@ class Graph {
     }
   }
 
-  // bfs(v)
+  // function to performs BFS
+  bfs(startingNode) {
+
+    // create a visited object
+    var visited = {};
+
+    // Create an object for queue
+    var q = new Queue();
+
+    // add the starting node to the queue
+    visited[startingNode] = true;
+    q.enqueue(startingNode);
+
+    // loop until queue is empty
+    while (!q.isEmpty()) {
+      // get the element from the queue
+      var getQueueElement = q.dequeue();
+
+      // passing the current vertex to callback function
+      console.log(getQueueElement);
+
+      // get the adjacent list for current vertex
+      var get_List = this.AdjList.get(getQueueElement);
+
+      // loop through the list and add the element to the
+      // queue if it is not processed yet
+      for (var i in get_List) {
+        var neighbour = get_List[i];
+
+        if (!visited[neighbour]) {
+          visited[neighbour] = true;
+          q.enqueue(neighbour);
+        }
+      }
+    }
+  }
   // dfs(v)
 }
+
+module.exports = { Graph }
---
+++ Data-Structures/Graph/main.js
@@ -0,0 +1,22 @@
+const { Graph } = require('./Graph-AdjList');
+
+// Using the above implemented graph class
+var g = new Graph(6);
+var vertices = [ 'A', 'B', 'C', 'D', 'E', 'F' ];
+ 
+// adding vertices
+for (var i = 0; i < vertices.length; i++) {
+    g.addVertex(vertices[i]);
+}
+ 
+// adding edges
+g.addEdge('A', 'B');
+g.addEdge('A', 'D');
+g.addEdge('A', 'E');
+g.addEdge('B', 'C');
+g.addEdge('D', 'E');
+g.addEdge('E', 'F');
+g.addEdge('E', 'C');
+g.addEdge('C', 'F');
+
+g.printGraph();
