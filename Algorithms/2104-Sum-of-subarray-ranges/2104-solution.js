dae0b3fb220f8787768128998ce8b0eef7a10b37
Muhammad Majid <mmajidkamel@gmail.com>
2022-06-02 20:10:14 +0200

    init problem 2104

---
+++ Algorithms/2104-Sum-of-subarray-ranges/2104-solution.js
@@ -0,0 +1,10 @@
+/**
+ * The idea is:
+ * for each element in the array, find the No. of subarrays that element is a Max and a Min.
+ * Answer = Sum(No. of subarrays elem is Max * elem) - Sum(No. of subarrays elem is Min * elem)
+ */
+
+ var subArrayRanges = function(nums) {
+
+
+}; 
