0e36518adf4014934f883c7333aa2e7b77cc78ec
Muhammad Majid <mmajidkamel@gmail.com>
2022-06-07 23:03:23 +0200

    added solution

---
+++ Algorithms/1567-Maximum-len-subarray-with-positive-product/solution.js
@@ -0,0 +1,14 @@
+function getMaxLen(nums) {
+  let max = 0, pos = 0, neg = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 0) {
+      [pos, neg] = [0, 0];
+    } else if (nums[i] < 0) {
+      [pos, neg] = [neg > 0 ? neg + 1 : 0, pos + 1];
+    } else {
+      [pos, neg] = [pos + 1, neg > 0 ? neg + 1 : 0];
+    }
+    max = Math.max(max, pos);
+  }
+  return max;
+}
