ef9af13f723ae733eaf55404b3ea5d3be4e0e039
Muhammad Majid <mmajidkamel@gmail.com>
2022-08-31 14:57:45 +0200

    Added solution for converting linkedlist to BST

---
+++ Algorithms/109-convert-sorted-list-BST/solution.js
@@ -0,0 +1,18 @@
+var sortedListToBST = function(head) {
+	const arr = []
+	while(head){
+		arr.push(head.val)
+		head = head.next
+	}
+
+	function bst(arr,left,right){
+		if(left > right) return null
+		const mid = Math.floor((left+right)/2)
+		const tree = new TreeNode(arr[mid])
+		tree.left = bst(arr,left,mid-1)
+		tree.right = bst(arr,mid+1,right)
+		return tree
+	}
+
+	return bst(arr,0,arr.length-1)
+};
