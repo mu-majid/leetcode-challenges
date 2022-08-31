a07586b6616a1b6037865b8fb0f7b38eebe7db15
Muhammad Majid <mmajidkamel@gmail.com>
2022-08-31 14:58:51 +0200

    Update solution.js

--- Algorithms/109-convert-sorted-list-BST/solution.js
+++ Algorithms/109-convert-sorted-list-BST/solution.js
@@ -1,18 +1,19 @@
 var sortedListToBST = function(head) {
-	const arr = []
+	const arr = [];
+  //convert ll to arr
 	while(head){
-		arr.push(head.val)
-		head = head.next
-	}
+		arr.push(head.val);
+		head = head.next;
+	};
 
 	function bst(arr,left,right){
-		if(left > right) return null
-		const mid = Math.floor((left+right)/2)
-		const tree = new TreeNode(arr[mid])
-		tree.left = bst(arr,left,mid-1)
-		tree.right = bst(arr,mid+1,right)
-		return tree
+		if(left > right) return null;
+		const mid = Math.floor((left+right)/2);
+		const tree = new TreeNode(arr[mid]);
+		tree.left = bst(arr,left,mid-1);
+		tree.right = bst(arr,mid+1,right);
+		return tree;
 	}
 
-	return bst(arr,0,arr.length-1)
+	return bst(arr,0,arr.length-1);
 };
