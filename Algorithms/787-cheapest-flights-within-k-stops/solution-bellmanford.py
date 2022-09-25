8a10879708f37a8738e691828610b29f86f1f535
Muhammad Majid <mmajidkamel@gmail.com>
2022-09-26 00:32:57 +0200

    cheapest flight dag

---
+++ Algorithms/787-cheapest-flights-within-k-stops/solution-bellmanford.py
@@ -0,0 +1,7 @@
+from ast import List
+
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float("inf")] * n
+        prices[src] = 0
